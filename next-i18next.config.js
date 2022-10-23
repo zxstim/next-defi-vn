e9729fd1570faa9a99bbeff5f451164e7d9b0da9
Victor Luong <thang.victor.luong@gmail.com>
2022-10-23 20:53:53 +0700

    removed reload

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,7 +4,6 @@ module.exports = {
     i18n: {
       defaultLocale: 'vi',
       locales: ['vi', 'en'],
-      reloadOnPrerender: true,
       localeDetection: true,
       localePath: path.resolve('./public/locales')
     },

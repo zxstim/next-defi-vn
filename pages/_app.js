d67d19a9ac604aef795ea270873b1540cd0b8836
Victor Luong <thang.victor.luong@gmail.com>
2022-10-23 21:18:04 +0700

    fixed loading spinner

--- pages/_app.js
+++ pages/_app.js
@@ -9,8 +9,8 @@ import Router from 'next/router';
 NProgress.configure({
   minimum: 0.3,
   easing: 'ease',
-  speed: 800,
-  showSpinner: false,
+  speed: 500,
+  showSpinner: true,
 });
 
 Router.events.on('routeChangeStart', () => NProgress.start());

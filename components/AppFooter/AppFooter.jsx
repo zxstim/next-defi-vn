7388cf04e02b8084f191e4eca730f0ed23fe88ed
Victor Luong <thang.victor.luong@gmail.com>
2022-11-09 23:34:08 +0700

    added disclaimer

--- components/AppFooter/AppFooter.jsx
+++ components/AppFooter/AppFooter.jsx
@@ -11,27 +11,28 @@ export default function AppFooter() {
         <a href="https://t.me/zxstim">@zxstim</a>
       </h3>
       <span>
-          <a href="https://github.com/0xstim">
-            <img src="/icons8-github.svg" alt="Github SVG" />
+        <a href="https://github.com/0xstim">
+          <img src="/icons8-github.svg" alt="Github SVG" />
+        </a>
+      </span>
+      <span>
+        <a href="https://twitter.com/0xstim">
+          <img src="/icons8-twitter.svg" alt="Twitter SVG" />
+        </a>
+      </span>
+      <span>
+        <a href="https://t.me/zxstim">
+          <img src="/icons8-telegram.svg" alt="Telegram SVG" />
+        </a>
+      </span>
+      <span>
+        <Link href="/0xstim">
+          <a>
+            <img src="/icons8-info.svg" alt="Website PNG" />
           </a>
-        </span>
-        <span>
-          <a href="https://twitter.com/0xstim">
-            <img src="/icons8-twitter.svg" alt="Twitter SVG" />
-          </a>
-        </span>
-        <span>
-          <a href="https://t.me/zxstim">
-            <img src="/icons8-telegram.svg" alt="Telegram SVG" />
-          </a>
-        </span>
-        <span>
-          <Link href="/0xstim">
-            <a>
-              <img src="/icons8-info.svg" alt="Website PNG" />
-            </a>
-          </Link>
-        </span>
+        </Link>
+      </span>
+      <h3><Link href="/disclaimer"><a>{t("disclaimer")}</a></Link></h3>
       <h3>{t("donate")}</h3>
       <p>BTC: bc1q33f3fq37qstrptwd9xcrdgxemrxnkeqzrtjds8</p>
       <p>ETH: 0x052F71E171b11bc037999B2341fCa73f4534D8A5</p>
---
+++ pages/disclaimer.js
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import UpButton from "../components/UpButton/UpButton";
+import AppFooter from "../components/AppFooter/AppFooter";
+import DevsList from "../components/ForHire/DevsList";
+
+export default function ZxStim(props) {
+  const { t } = useTranslation("disclaimer");
+  return (
+    <>
+      <div className="App">
+        <div className="markdown-body">
+          <h1 id="top">{t("title")}</h1>
+          <div style={{ display: "flex", marginBottom: "10px" }}>
+            <Link href="/en/disclaimer" locale="en">
+              <a style={{ textDecoration: "none" }}>
+                <p className="i18n-button">🇬🇧</p>
+              </a>
+            </Link>
+            <Link href="/disclaimer" locale="vi">
+              <a style={{ textDecoration: "none" }}>
+                <p className="i18n-button">🇻🇳</p>
+              </a>
+            </Link>
+          </div>
+          <Link href="/">{t("back")}</Link>
+          <UpButton />
+          <h2>{t("header1")}</h2>
+          <p>{t("content1")}</p>
+          <p>{t("content2")}</p>
+          <p>{t("content3")}</p>
+          <br />
+          <hr />
+          <AppFooter />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common", "disclaimer"])),
+      // Will be passed to the page component as props
+    },
+  };
+}
